cfb8a5f963bc7f32e1925efabf9002578c7b7910
Lingchao Xin <douglarek@gmail.com>
2014-02-25 00:48:09 +0800

    Add pyenv configuration and enable plugins

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme l
+
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+set fish_plugins pyenv
+
+# Path to your custom folder (default path is $FISH/custom)
+set fish_custom (dirname $fish_function_path[1])/custom $FISH/custom
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
